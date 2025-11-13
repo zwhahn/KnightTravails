57d4fa868e56fd35d42382a5fe64d371c9ae08be
zwhahn <117476820+zwhahn@users.noreply.github.com>
2025-11-13 16:09:46 -0500

    Try to add recursive method similar to tree building to knightMoves, it's not finding a solution
    Create getNextMoves to generate all next legal moves from a position

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
 function knightMoves ([xStart, yStart], [xEnd, yEnd]) {
-    nextMoves = getNextMoves([xStart, yStart]);
+    let start = new Node([xStart, xEnd]);
+    const end = new Node([xEnd, yEnd]);
+    let queue = [start];
+    let visitedSquares = [start];
+
+    while (queue.length > 0) {
+        let current = queue.shift()
+        // console.log("current: ", current.position)
+
+        if (current.position == end.position) {
+            // console.log("Found Path")
+            return constructPath(current);
+        }
+
+        let nextMoves = getNextMoves(current.position);
+        // console.log("next moves:", nextMoves)
+
+        for (let pos of nextMoves) {
+            let move = new Node(pos, current.position);
+            if (!visited(visitedSquares, move)) {
+                visitedSquares.push(move);
+                queue.push(move);
+            }
+        }
+    }
+
 }
 
 function getNextMoves ([xNode, yNode]) {
@@ -7,36 +32,54 @@ function getNextMoves ([xNode, yNode]) {
 
     // Check legal moves add to move array
     if (xNode - 1 >= 0 && yNode + 2 <= 7) {
-        moves.push([xNode - 1], (yNode + 2));
+        moves.push([xNode - 1, yNode + 2]);
     }
 
     if (xNode - 2 >= 0 && yNode + 1 <= 7){
-        moves.push([xNode - 2], (yNode + 1));
+        moves.push([xNode - 2, yNode + 1]);
     } 
 
     if (xNode - 2 >= 0 && yNode - 1 >= 0){
-        moves.push([xNode - 2], (yNode - 1));
+        moves.push([xNode - 2, yNode - 1]);
     }
 
     if (xNode - 1 >= 0 && yNode - 2 >= 0){
-        moves.push([xNode - 1], (yNode - 2));
+        moves.push([xNode - 1, yNode - 2]);
     }
 
     if (xNode + 1 <= 7 && yNode - 2 >= 0){
-        moves.push([xNode + 1], (yNode - 2));
+        moves.push([xNode + 1, yNode - 2]);
     }
 
     if (xNode + 2 <= 7 && yNode + 1 >= 0){
-        moves.push([xNode + 2], (yNode - 1));
+        moves.push([xNode + 2, yNode - 1]);
     }
 
     if (xNode + 2 <= 7 && yNode + 1 <= 7){
-        moves.push([xNode + 2], (yNode + 1));
+        moves.push([xNode + 2, yNode + 1]);
     }
 
     if (xNode + 1 <= 7 && yNode + 2 <= 7){
-        moves.push([xNode + 1], (yNode + 2));
+        moves.push([xNode + 1, yNode + 2]);
     }
 
     return moves;
 }
+
+function visited (visitedSquares, move) {
+    for (node in visitedSquares) {
+        if (node.position == move.position) {
+            return true
+        }
+    }
+    return false
+}
+
+class Node {
+    constructor (position = null, parent = null) {
+        this.position = position;
+        this.parent = parent;
+    }
+}
+
+knightMoves([3,3],[4,3])
